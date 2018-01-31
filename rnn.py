35a73ff3e37cd172448aad4cb729b444bff69bdd
Bansal <shubansa@acbc32b81227.ant.amazon.com>
2018-01-31 21:15:03 +0530

    imported training set, feature scaling and 60 timesteps datastructure

--- rnn.py
+++ rnn.py
@@ -9,6 +9,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# Importing the training set
+dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
+training_set = dataset_train.iloc[:, 1:2].values
+
+# Feature Scaling --- We apply normalization [(x-min)/(max-min)]
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0, 1))
+training_set_scaled = sc.fit_transform(training_set)
+
+# Creating a data structure with 60 timesteps and 1 output
+# 60 time steps means at each time t the rnn is going to look at the 60 stock prices before time t i.e. 
+# the stock prices between 60 days before t and t and based on the trends it capture it will predict the 1 output
+# each month has 20 financial days. so 60 days means 3 months. we look back 3 months everytime
+
+X_train = []
+y_train = []
+for i in range(60, 1258):
+    X_train.append(training_set_scaled[i-60:i, 0])
+    y_train.append(training_set_scaled[i, 0])
+    
+X_train, y_train = np.array(X_train), np.array(y_train)
+
+    
+
+
 
 # Part 2 - Building the RNN
 
